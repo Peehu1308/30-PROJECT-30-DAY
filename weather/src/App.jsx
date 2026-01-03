57fe2659ed8b4aaf79f164a45b3a7074042f967a
Peehu Mishra <peehumishra1308@gmail.com>
2026-01-04 00:04:14 +0530

    Create App.jsx

---
+++ weather/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Weather from './components/weather'
+
+function App() {
+
+
+  return (
+    <div className='bg-black text-white min-h-screen'>
+      <Weather/>
+      
+    </div>
+  )
+}
+
+export default App
